4ae9194051148272f28b0a0266065a7dd1f0bbc6
KarenQuezadaMejia <quezadakaren16@gmail.com>
2019-05-27 16:10:49 -0400

    inicio

---
+++ src/main.js
@@ -0,0 +1,17 @@
+//$('body').scrollspy({ target: '#navbar-example2' })
+
+/*var nav = document.getElementById('navbar-example2');
+
+window.onscroll = function () {
+
+  if(window.pageYOffset > 500){
+
+   nav.style.position = "absolute";
+   nav.style.top = 0;
+
+   }else{
+     // nav.style.position = "absolute";
+     nav.style.position = 'fixed'; //fixed
+     nav.style.top = 100;
+   }
+}
