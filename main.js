8f20529af58181a38a2c197bf61a8955b4e2ff0b
KarenQuezadaMejia <quezadakaren16@gmail.com>
2019-05-31 14:41:45 -0400

    Updates

---
+++ main.js
@@ -0,0 +1,25 @@
+//$('body').scrollspy({ target: '#navbar-example2' })
+
+/*var nav = document.getElementById('navbar-example2');
+
+window.onscroll = function () {
+
+  if(window.pageYOffset > 500){
+
+   nav.style.position = "absolute";
+   nav.style.top = 0;
+
+   }else{
+     // nav.style.position = "absolute";
+     nav.style.position = 'fixed'; //fixed
+     nav.style.top = 100;
+   }
+}*/
+
+//SCROLLSPY
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+
+      
